749d22ad86a9070da3f88da558c5db2eafa22671
glmsr12 <gulumsercakmak@hotmail.com>
2022-12-18 16:20:21 -0800

    "calculation on expense, income and balance"

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -5,7 +5,9 @@ const Balance = () => {
   const { transactions } = useContext(GlobalContext);
 
   const amounts = transactions.map((transaction) => transaction.amount);
-  const total = amounts.reduce((acc, item) => (acc += item), 0);
+  const total = amounts.reduce((acc, item) => acc - -item, 0).toFixed(2);
+
+  console.log(total);
 
   return (
     <>
